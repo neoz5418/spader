c1a5f9e18b7fef7a5d25f0baf9dc41a761965d07
Zhengyi Lai <zheng1@kubesphere.io>
2024-11-21 16:37:40 +0800

    refactor

--- frontend/src/pages/instances/index.tsx
+++ frontend/src/pages/instances/index.tsx
@@ -1,68 +1,64 @@
 import { Layout } from '@/components/custom/layout'
 
 
-import { useListInstancesHook as useReadInstancesHook, InstanceType, useListWorkspaceInstancesHook, WorkspaceType, listWorkspaceInstances, PaginatedListInstanceType } from '@/gen'
+import { InstanceType, listWorkspaceInstances, PaginatedListInstanceType, WorkspaceType } from '@/gen'
 import { useMemo, useState } from 'react'
-import { ColumnDef } from "@tanstack/react-table";
-import { DataTable, DataError } from "@/components/custom/data-table";
-import Loader from '@/components/loader';
-import useWorkspace from '@/hooks/use-setting';
-import { Button } from '@/components/custom/button'
-import negateValue from '../../../../frontend-shadcn/node_modules/tailwindcss/src/util/negateValue';
-import { useNavigate, Link } from 'react-router-dom';
-import WorkspaceLayout from '../../../../../../spader-ai/spader-console/src/app/(authed)/workspaces/[workspace]/layout';
-import { useQuery } from '@tanstack/react-query';
+import { ColumnDef } from '@tanstack/react-table'
+import { DataTable } from '@/components/custom/data-table'
+import Loader from '@/components/loader'
+import useWorkspace from '@/hooks/use-setting'
+import { useQuery } from '@tanstack/react-query'
 
 
 export const columns: ColumnDef<InstanceType>[] = [
   {
-    accessorKey: "uid",
-    header: "ID",
+    accessorKey: 'uid',
+    header: 'ID',
   },
   {
-    accessorKey: "name",
-    header: "Name",
+    accessorKey: 'name',
+    header: 'Name',
   },
   {
-    accessorKey: "display_name",
-    header: "Display Name",
+    accessorKey: 'display_name',
+    header: 'Display Name',
   },
   {
-    accessorKey: "status",
-    header: "Status",
+    accessorKey: 'status',
+    header: 'Status',
   },
   {
-    accessorKey: "zone",
-    header: "Zone",
+    accessorKey: 'zone',
+    header: 'Zone',
   },
   {
-    accessorKey: "workspace",
-    header: "Workspace",
+    accessorKey: 'workspace',
+    header: 'Workspace',
   },
   {
-    accessorKey: "gpu_count",
-    header: "GPU Count",
+    accessorKey: 'gpu_count',
+    header: 'GPU Count',
   },
   {
-    accessorKey: "gpu_type",
-    header: "GPU Type",
+    accessorKey: 'gpu_type',
+    header: 'GPU Type',
   },
   {
-    accessorKey: "image",
-    header: "Image",
+    accessorKey: 'image',
+    header: 'Image',
   },
   {
-    accessorKey: "create_time",
-    header: "Created At",
-  }
+    accessorKey: 'create_time',
+    header: 'Created At',
+  },
 ]
 
 export default function Instances() {
-  const {workspace:currentWorkspace} = useWorkspace()
+  const { workspace: currentWorkspace } = useWorkspace()
   const [pagination, setPagination] = useState({
     pageIndex: 0, //initial page index
     pageSize: 10, //default page size
-  });
+  })
   // const workspace = ws as WorkspaceType
   console.log(currentWorkspace)
   // if (!workspace) {
@@ -82,13 +78,13 @@ export default function Instances() {
   //   offset: pagination.pageSize * pagination.pageIndex,
   // });
 
-  const { isLoading: isLoading, data: data, error } = useQuery<PaginatedListInstanceType >({
-    queryKey: ["instances", currentWorkspace?.name],
-    queryFn: async () =>{
+  const { isLoading: isLoading, data: data, error } = useQuery<PaginatedListInstanceType>({
+    queryKey: ['instances', currentWorkspace?.name],
+    queryFn: async () => {
       const workspaceName = (currentWorkspace as WorkspaceType).name
-      console.log("[+]listWorkspaceInstances", workspaceName)
+      console.log('[+]listWorkspaceInstances', workspaceName)
       const response = await listWorkspaceInstances(workspaceName)
-      console.log("[+]listWorkspaceInstances", response)
+      console.log('[+]listWorkspaceInstances', response)
       return response
     },
     enabled: !!currentWorkspace,
@@ -106,17 +102,17 @@ export default function Instances() {
         gpu_count: instance.gpu_count,
         gpu_type: instance.gpu_type,
         image: instance.image,
-        create_time: instance.create_time
+        create_time: instance.create_time,
       })),
-    [data]
-  );
+    [data],
+  )
 
   if (isLoading) {
     return <Loader />
   }
 
 
-  console.log("[+]error", error)
+  console.log('[+]error', error)
   // if (error) {
   //   return <DataError {...error} />;
   // }
@@ -124,18 +120,19 @@ export default function Instances() {
   return (
     <Layout>
       <Layout.Body>
-        <div className='mb-2 flex items-center justify-between space-y-2'>
+        <div className="mb-2 flex items-center justify-between space-y-2">
           <div>
-            <h2 className='text-2xl font-bold tracking-tight'>算力容器列表</h2>
-            <p className='text-muted-foreground'>
+            <h2 className="text-2xl font-bold tracking-tight">算力容器列表</h2>
+            <p className="text-muted-foreground">
 
             </p>
           </div>
         </div>
-        <div className='-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0'>
-            <DataTable columns={columns} data={instances} createLink={`/workspaces/${currentWorkspace?.name}/instances/deploy`}
-            rowCount={data?.pagination?.total ?? 0}
-            pagination={pagination} setPagination={setPagination} />
+        <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <DataTable columns={columns} data={instances}
+                     createLink={`/workspaces/${currentWorkspace?.name}/instances/deploy`}
+                     rowCount={data?.pagination?.total ?? 0}
+                     pagination={pagination} setPagination={setPagination} />
         </div>
       </Layout.Body>
     </Layout>

0a57839d08fbc5f728d6f662a992bc8b133ed44e
Zhengyi Lai <zheng1@kubesphere.io>
2024-11-22 09:55:59 +0800

    bugfix: loading error

--- frontend/src/gen/zod/userQuotaSchema.gen.ts
+++ frontend/src/gen/zod/userQuotaSchema.gen.ts
@@ -1,5 +1,5 @@
 import { z } from "@/utils/zod.ts";
 
 
-export const userQuotaSchema = z.object({ "username": z.string(), "limitation": z.object({}).catchall(z.number().int()).default([object, Object]).optional(), "status": z.object({}).catchall(z.number().int()).default([object, Object]).optional() });
+export const userQuotaSchema = z.object({ "username": z.string(), "limitation": z.object({}).catchall(z.number().int()).default({}).optional(), "status": z.object({}).catchall(z.number().int()).default({}).optional() });
 export type UserQuotaSchema = z.infer<typeof userQuotaSchema>;

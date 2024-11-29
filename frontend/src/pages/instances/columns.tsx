e28b998edf68775e91c997d854706bba7154b4c3
Zhengyi Lai <zheng1@kubesphere.io>
2024-11-29 13:04:22 +0800

    Display service

--- frontend/src/pages/instances/columns.tsx
+++ frontend/src/pages/instances/columns.tsx
@@ -1,5 +1,10 @@
 import { ColumnDef } from '@tanstack/react-table'
 import { InstanceType } from '@/gen'
+import { toast } from '@/hooks/use-toast.ts'
+
+function SelectorAble({ children }: { children: React.ReactNode }) {
+  return <span className={"select-all cursor-pointer"}>{children}</span>
+}
 
 export const InstancesColumns: ColumnDef<InstanceType>[] = [
   {
@@ -20,22 +25,51 @@ export const InstancesColumns: ColumnDef<InstanceType>[] = [
     header: '区域',
   },
   {
-    accessorKey: 'gpu_count',
-    header: 'GPU 数量',
-  },
-  {
-    accessorKey: 'gpu_type',
-    header: 'GPU 类型',
+    accessorFn: (row) => {
+      return `${row.gpu_type}*${row.gpu_count}`
+    },
+    header: 'GPU 类型*数量',
   },
   {
     accessorKey: 'image',
     header: '镜像',
   },
   {
+    cell: ({ row }) => {
+      const services = []
+      if (row.original.services) {
+        if (row.original.services['jupyter-lab']) {
+          services.push(<div>Jupyter 地址: <SelectorAble>{row.original.services['jupyter-lab']}</SelectorAble></div>)
+        }
+        if (row.original.services['jupyter-password']) {
+          services.push(
+            <div>
+              Jupyter 密码:
+              <span
+                className="select-all cursor-pointer"
+                onClick={() => {
+                  navigator.clipboard.writeText(row.original.services['jupyter-password'] || '')
+                  toast(<p>密码已复制到剪贴板</p>)
+                }}
+              >
+                {row.original.services['jupyter-password']}
+              </span>
+            </div>,
+          )
+          if (row.original.services['ssh']) {
+            services.push(<div>SSH 地址: <SelectorAble>{row.original.services['ssh']}</SelectorAble></div>)
+          }
+        }
+        return <div>{services}</div>
+      }
+    },
+    header: '服务',
+  },
+  {
     accessorFn: (row) => {
-      const date = new Date(row.create_time || new Date());
-      date.setHours(date.getHours() + 8);
-      return date.toLocaleString();
+      const date = new Date(row.create_time || new Date())
+      date.setHours(date.getHours() + 8)
+      return date.toLocaleString()
     },
     header: '创建时间',
   },

6ff19572dc83035554d788a98bcbeadbfb2aa7d0
Zhengyi Lai <zheng1@kubesphere.io>
2024-12-30 19:06:36 +0800

    Change default theme to system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,20 @@
-import { RouterProvider } from 'react-router-dom'
-import { Toaster } from '@/components/ui/toaster'
-import { ThemeProvider } from '@/components/theme-provider'
-import router from '@/router'
-import '@/index.css'
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import router from "@/router";
+import { RouterProvider } from "react-router-dom";
+import "@/index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import QueryProvider from '@/components/providers'
-
+import QueryProvider from "@/components/providers";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-      <QueryProvider>
-        <RouterProvider router={router} />
-        <Toaster />
-      </QueryProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+			<QueryProvider>
+				<RouterProvider router={router} />
+				<Toaster />
+			</QueryProvider>
+		</ThemeProvider>
+	</React.StrictMode>,
 );
